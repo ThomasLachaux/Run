525a07c82238a9ec19372959199f7eab03fa6ade
Herel Adrastel <herel.dev@gmail.com>
2019-04-29 14:55:02 -0400

    Trying portable way, not finished

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 ﻿# SpaceShooter
+
+set "PATH=%PATH%;C:\Program Files (x86)\CodeBlocks\MinGW\bin;"
+mkdir build
+cmake -DCMAKE_BUILD_TYPE=Debug -DCMAKE_SH=CMAKE_SH-NOTFOUND -G "CodeBlocks - MinGW Makefiles" ..
+mingw32-make
---
+++ build/CMakeFiles/SpaceShooter.dir/cmake_clean.cmake
@@ -0,0 +1,15 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/SpaceShooter.dir/src/main.c.obj"
+  "CMakeFiles/SpaceShooter.dir/src/game.c.obj"
+  "CMakeFiles/SpaceShooter.dir/src/body.c.obj"
+  "CMakeFiles/SpaceShooter.dir/src/world.c.obj"
+  "../output/SpaceShooter.pdb"
+  "../output/SpaceShooter.exe"
+  "../output/SpaceShooter.exe.manifest"
+  "libSpaceShooter.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/SpaceShooter.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
