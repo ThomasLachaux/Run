0600773460570705d3408f2946c2c4bb7c563782
Herel Adrastel <herel.dev@gmail.com>
2019-05-09 17:48:41 -0400

    Code Blocks updated

--- src/game.c
+++ src/game.c
@@ -19,7 +19,7 @@ Game initGame() {
     game.bigFont = TTF_OpenFont(ASSETS("LemonMilk.ttf"), 50);
 
 
-    game.window = SDL_CreateWindow("Space Shooter", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
+    game.window = SDL_CreateWindow("Run !", SDL_WINDOWPOS_UNDEFINED, SDL_WINDOWPOS_UNDEFINED, SCREEN_WIDTH, SCREEN_HEIGHT, SDL_WINDOW_SHOWN);
 
     game.renderer = SDL_CreateRenderer(game.window, -1, SDL_RENDERER_ACCELERATED);
 
